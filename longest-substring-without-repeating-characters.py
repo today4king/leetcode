cd77e385f5d8496b3062717e5fb23a378344b050
carry <carry@moyu.team>
2019-05-05 19:58:08 +0800

    first commit

---
+++ longest-substring-without-repeating-characters.py
@@ -0,0 +1,39 @@
+from memory_profiler import profile
+
+
+
+class Solution:
+    def indexOf(self,input_s:str,input_c: chr)->int:
+        for i,c in enumerate(input_s):
+            if c == input_c:
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        MaxL = 0
+        ss = []
+        for i, c in enumerate(s):
+
+            if ss.__contains__(c):
+                repeat_i = ss.index(c)
+                ss = ss[repeat_i + 1:]
+                ss.append(c)
+            else:
+                ss.append(c)
+            if len(ss) > MaxL:
+                MaxL = len(ss)
+        return MaxL
+
+@profile
+def inspect(s, l):
+    L = Solution().lengthOfLongestSubstring(s)
+    print('%s\tLength %d\tEspected %d' % (L == l, L, l))
+
+
+if __name__ == "__main__":
+    print(Solution().lengthOfLongestSubstring("abcabcbb") == 3)
+    print(Solution().lengthOfLongestSubstring("bbbbb") == 1)
+    print(Solution().lengthOfLongestSubstring("pwwkew") == 3)
+    print(Solution().lengthOfLongestSubstring("dvdf") == 3)
+    print(Solution().lengthOfLongestSubstring(" ") == 1)
+    inspect("aabaab!bb", 3)
+
+# 10.5MB
