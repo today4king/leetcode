413f04cca6de468aae1355d02c4cfc30130d1d24
unknown <today4king@gmail.com>
2021-04-30 13:51:55 +0800

    add all

--- zhi-xing.py
+++ zhi-xing.py
@@ -40,20 +40,36 @@ class Solution:
         stock_picked = sort_value(stock_picked, reverse=True)
 
         ep = 9999
+        # 此flag用于花光用户袋袋里头的最后一块钱
+        has_bought_flag = False
+        len_stock_picked = len(stock_picked)
+        i = 0
+        force_bought = False
         while ep > 0:
             for stock_name in stock_picked.keys():
                 # 他还有钱且当前此股票比例小于他期望的比例则购买一手
                 if pocket_money - stock_trade_unit_dic[stock_name] > 0 \
-                        and stock_bought_proportion[stock_name] < stock_picked[stock_name]:
+                        and (stock_bought_proportion[stock_name] < stock_picked[stock_name] or force_bought):
                     # 口袋里的钱又少了
                     pocket_money -= stock_trade_unit_dic[stock_name]
                     stock_bought_dic[stock_name] += 1
+                    has_bought_flag = True
+                    if force_bought:
+                        force_bought = False
                     # 重新计算意愿
                     stock_money = input_money - pocket_money
                     for k, v in stock_bought_proportion.items():
                         stock_bought_proportion[k] = stock_bought_dic[k] * 100 * stock_price_dic[k] / stock_money
                 if pocket_money < min_bought:
                     return stock_bought_dic, stock_bought_proportion, pocket_money
+                i += 1
+
+            # 当确定没有意愿购买时强制购买
+            if i % len_stock_picked == 0:
+                if has_bought_flag == False:
+                    force_bought = True
+                else:
+                    has_bought_flag = False
             ep += 1
 
 
@@ -62,6 +78,12 @@ if __name__ == "__main__":
     s = {'美团': 0.2, '腾讯': 0.4, '茅台': 0.4}
     print(s)
     print(solution.stock_advise(50000, s))
+    print('-----------------50000------------------')
+    s = {'美团': 0.2, '腾讯': 0.4, '茅台': 0.4}
+    print(s)
+    print(solution.stock_advise(15000, s))
+    print('----------------15000-------------------')
     s = {'美团': 0.1, '腾讯': 0.4, '茅台': 0.4}
     print(s)
     print(solution.stock_advise(5000, s))
+    print('----------------5000-------------------')
