e0431d521d1dec8488028e62c0b06abcb131a4de
carry <carry@moyu.team>
2021-05-02 14:27:01 +0800

    stash

--- 15-3sum.py
+++ 15-3sum.py
@@ -4,19 +4,47 @@
 #
 from typing import *
 
+
 class Solution:
+    coodinate_list=None
+    def sort(self, nums,i,j):
+        while j>i:
+
+
+    def has_k(self, i, j, k_value, nums):
+        while j > i + 1:
+            k = (j + i) // 2
+            if nums[k] == k_value:
+                return k
+            elif nums[k] > k_value:
+                i = k
+            else:
+                j = k
+        return -1
+
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums_l=len(nums)
-        if nums_l<3:
+        nums_l = len(nums)
+
+        if nums_l < 3:
             return []
+        self.coodinate_list=[x for x in range(len(nums))]
+        sorted_nums = self.sort(nums,0,nums_l-1)
+        i = 0
+        j = 1
+        ret = []
+        while j > i + 1:
+            k = self.has_k(i, j, 0 - sorted_nums[i] - sorted_nums[j], sorted_nums)
+            if k > 0:
+                ret.append(i, k, j)
+        return ret
 
 
 if __name__ == "__main__":
     solution = Solution()
-    nums = [-1,0,1,2,-1,-4]
+    nums = [-1, 0, 1, 2, -1, -4]
     print(nums)
     print(solution.threeSum(nums))
-    print(solution.threeSum(nums) == [[-1,-1,2],[-1,0,1]])
+    print(solution.threeSum(nums) == [[-1, -1, 2], [-1, 0, 1]])
     print('-------------------------------------')
     nums = []
     print(nums)
