01031933e13e79fdff7a60bfed289ef4a6c831a9
carry <carry@moyu.team>
2021-05-02 22:30:17 +0800

    pass 15

--- 15-3sum.py
+++ 15-3sum.py
@@ -2,87 +2,95 @@
 #   #
 #   Authors: Carry Jin <today4king@gmail.com>
 #
+import random
 from typing import *
+
+from memory_profiler import profile
 
 
 class Solution:
 
     def has_k(self, i, j, k_value, nums):
-        ks = []
-        for n in nums[i+1:j]:
-            if n == k_value:
-                ks.append(n)
-        return ks
 
-    def sort(self, nums: List[int], start, end, coodinate_list):
-        is_from_right = True
+        for n in nums[i + 1:j]:
+            if n == k_value:
+                return True
+        return False
+
+    def swap(self, i, j, nums):
+        nums[j] += nums[i]
+        nums[i] = nums[j] - nums[i]
+        nums[j] = nums[j] - nums[i]
+        return nums
+
+    def quick_sort(self, start, end, nums):
+        if start < 0:
+            return nums
+        if end >= len(nums):
+            return nums
+        pivot = start
         i = start
         j = end
-        pivot = start
-        while j > i:
-            if is_from_right and nums[j] < nums[i]:
-                # swap number
-                nums[j] += nums[i]
-                nums[i] = nums[j] - nums[i]
-                nums[j] = nums[j] - nums[i]
-                # swap coodinate
-                coodinate_list[j] += coodinate_list[i]
-                coodinate_list[i] = coodinate_list[j] - coodinate_list[i]
-                coodinate_list[j] = coodinate_list[j] - coodinate_list[i]
-                i += 1
-                is_from_right = False
-                pivot = j
-            if not is_from_right and nums[i] > nums[j]:
-                # swap
-                nums[j] += nums[i]
-                nums[i] = nums[j] - nums[i]
-                nums[j] = nums[j] - nums[i]
-                # swap coodinate
-                coodinate_list[j] += coodinate_list[i]
-                coodinate_list[i] = coodinate_list[j] - coodinate_list[i]
-                coodinate_list[j] = coodinate_list[j] - coodinate_list[i]
-                j -= 1
-                is_from_right = True
-                pivot = i
-            if is_from_right:
-                j -= 1
+        # true from left, false from right
+        right = True
+        while j >= i:
+            if right:
+                if nums[j] < nums[pivot]:
+                    nums = self.swap(i, j, nums)
+                    pivot = j
+                    right = False
+                    i += 1
+                else:
+                    j -= 1
             else:
-                i += 1
-        if pivot > 1:
-            nums, coodinate_list = self.sort(nums, 0, pivot - 1, coodinate_list)
-        if pivot < end - 1:
-            nums, coodinate_list = self.sort(nums, pivot + 1, end, coodinate_list)
-        return nums, coodinate_list
+                if nums[i] > nums[pivot]:
+                    nums = self.swap(i, j, nums)
+                    pivot = i
+                    right = True
+                    j -= 1
+                else:
+                    i += 1
+        if end > start:
+            nums = self.quick_sort(start, pivot - 1, nums)
+            nums = self.quick_sort(pivot + 1, end, nums)
+        return nums
 
-    def find_match(self, i, j, sorted_nums, coodinate_list):
-        ret = []
-        while j > i + 1:
-            kv = 0 - sorted_nums[i] - sorted_nums[j]
-            if kv > nums[j]:
-                j -= 1
-            elif kv < nums[i]:
-                i += 1
-            else:
-                ks = self.has_k(i, j, kv, sorted_nums)
-                for k in ks:
-                    ret.append([coodinate_list[i], coodinate_list[k], coodinate_list[j]])
-                for c in self.find_match(i + 1, j, sorted_nums, coodinate_list):
-                    ret.append(c)
-                for c in self.find_match(i, j - 1, sorted_nums, coodinate_list):
-                    ret.append(c)
-                i += 1
-                j -= 1
-        return ret
+    def my_sort(self, nums: List[int]):
+        return self.quick_sort(0, len(nums) - 1, nums)
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+
         nums_l = len(nums)
         if nums_l < 3:
             return []
-        coodinate_list = [x for x in range(len(nums))]
-        sorted_nums, coodinate_list = self.sort(nums, 0, nums_l - 1, coodinate_list)
+        sorted_nums = self.my_sort(nums)
+
+        print(sorted_nums)
         i = 0
-        j = nums_l - 1
-        return self.find_match(i, j, sorted_nums, coodinate_list)
+        ret = []
+        while i < nums_l - 2:
+            # 去除重复项
+            if i > 1 and sorted_nums[i] == sorted_nums[i - 1]:
+                i += 1
+                continue
+            # 排序后三个数中必定有负数
+            if sorted_nums[i] > 0:
+                break
+            x = i + 1
+            y = nums_l - 1
+            while y > x:
+                c = sorted_nums[x] + sorted_nums[y] + sorted_nums[i]
+                if c == 0:
+                    t = [sorted_nums[i], sorted_nums[x], sorted_nums[y]]
+                    ret.append(t)
+                    y -= 1
+                    x += 1
+                if c > 0:
+                    y -= 1
+                elif c < 0:
+                    x += 1
+            i += 1
+        return ret
 
 
 if __name__ == "__main__":
@@ -96,4 +104,17 @@ if __name__ == "__main__":
     print(nums)
     print(solution.threeSum(nums))
     print(solution.threeSum(nums) == [])
+    print('-------------------------------------')
+    nums = [0, 0, 0]
+    print(nums)
+    print(solution.threeSum(nums))
+    print(solution.threeSum(nums) == [[0, 0, 0]])
+    print('-------------------------------------')
+    nums = [34, 55, 79, 28, 46, 33, 2, 48, 31, -3, 84, 71, 52, -3, 93, 15, 21, -43, 57, -6, 86, 56, 94, 74, 83, -14, 28,
+            -66,
+            46, -49, 62, -11, 43, 65, 77, 12, 47, 61, 26, 1, 13, 29, 55, -82, 76, 26, 15, -29, 36, -29, 10, -70, 69, 17,
+            49]
+    print(nums)
+    print(solution.threeSum(nums))
+    print(solution.threeSum(nums) == [[0, 0, 0]])
     print('-------------------------------------')
